b1525e8bc3d07e3723a23e3eb1f7711cf421dbc2
bilal-webdeveloper <dallali-bilal@hotmail.fr>
2023-04-13 17:23:35 +0200

    welcome page

--- Test-technique/SpaceChooseView.swift
+++ Test-technique/SpaceChooseView.swift
@@ -9,7 +9,67 @@ import SwiftUI
 
 struct SpaceChooseView: View {
     var body: some View {
-        Text("Dude")
+        NavigationView {
+            VStack(spacing: 83) {
+                Text("Who is going to use\nthis device?")
+                    .foregroundColor(.white)
+                    .font(.custom("Abel-regular", size: 30))
+                    .padding(.bottom, 18)
+                    .multilineTextAlignment(.center)
+                VStack(spacing: 0) {
+                    NavigationLink {
+                        WelcomePageView()
+                    } label: {
+                        ZStack {
+                            HStack {
+                                Text("Parents")
+                                    .foregroundColor(Color("BackgroundColor"))
+                                    .font(.custom("Poppins-Bold", size: 24))
+                                    .padding(.leading, 54)
+                                Spacer()
+                            }
+                            .frame(height: 165)
+                            .frame(maxWidth: .infinity)
+                            .background(.white)
+                            .cornerRadius(12)
+                            HStack {
+                                Spacer()
+                                Image("parent-icon")
+                                    .padding(.bottom, 42)
+                            }
+                        }
+                    }
+                    
+                    NavigationLink {
+                        WelcomePageView()
+                    } label: {
+                        ZStack {
+                            HStack {
+                                Text("Child")
+                                    .foregroundColor(Color("BackgroundColor"))
+                                    .font(.custom("Poppins-Bold", size: 24))
+                                    .padding(.leading, 54)
+                                Spacer()
+                            }
+                            .frame(height: 173)
+                            .frame(maxWidth: .infinity)
+                            .background(.white)
+                            .cornerRadius(12)
+                            HStack {
+                                Spacer()
+                                Image("child-icon")
+                                    .padding(.bottom, 12)
+                            }
+                        }
+                    }
+
+                }
+                .padding(.horizontal, 47)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color("BackgroundColor"))
+        }
+        .navigationBarBackButtonHidden()
     }
 }
 
---
+++ Test-technique/WelcomePageView.swift
@@ -0,0 +1,45 @@
+//
+//  WelcomePageView.swift
+//  Test-technique
+//
+//  Created by Bilal Dallali on 13/04/2023.
+//
+
+import SwiftUI
+
+struct WelcomePageView: View {
+    var body: some View {
+        VStack(spacing: 40) {
+            Text("Welcom to 4IN Shield")
+                .foregroundColor(.white)
+                .font(.custom("Abel-regular", size: 30))
+                .padding(.bottom, 53)
+            Image("welcome-image")
+            Button {
+                //
+            } label: {
+                HStack {
+                    Text("Start")
+                        .foregroundColor(Color("FontColor"))
+                        .font(.custom("Abel-Regular", size: 18))
+                    Image(systemName: "chevron.right")
+                        .resizable()
+                        .frame(width: 6, height: 10)
+                        .foregroundColor(Color("BackgroundColor"))
+                }
+                .frame(width: 105, height: 50)
+                .background(.white)
+                .cornerRadius(8)
+            }
+
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color("BackgroundColor"))
+    }
+}
+
+struct WelcomePageView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomePageView()
+    }
+}
